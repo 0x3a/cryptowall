9d99209747d3b954a0ebf287aae8bbcd9fcafb92
Yonathan Klijnsma <admin@0x3a.com>
2015-11-09 11:17:34 +0100

    Added decrypter tool & updated read

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cryptowall
-Cryptowall Tooling &amp; Information
+# CryptoWall tooling / information
+This repository contains scripts / snippets created during my analysis of CryptoWall throughout the years.
+
+* cryptowall-post-decoder.py: allows you to decode the communication from infectees
---
+++ cryptowall-post-decoder.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+    CryptoWall C2 communication decoder by Yonathan Klijnsma (@ydklijnsma)
+
+    Works for:
+        - Unnamed cryptlocker clone (2013)
+        - CryptoDefense (2014)
+        - CryptoWall 1.0 (2014)
+        - CryptoWall 2.0 (2014)
+        - CryptoWall 3.0 (2015)
+"""
+
+import sys
+import zlib
+import hashlib
+
+def rc4(data, key):
+    x = 0
+    box = range(256)
+    for i in range(256):
+        x = (x + box[i] + ord(key[i % len(key)])) % 256
+        box[i], box[x] = box[x], box[i]
+    
+    x = y = 0
+    out = []
+    for char in data:
+        x = (x + 1) % 256
+        y = (y + box[x]) % 256
+        box[x], box[y] = box[y], box[x]
+        out.append(chr(ord(char) ^ box[(box[x] + box[y]) % 256]))    
+    return ''.join(out)
+    
+def decrypt_data(scrambled_key, data):
+    key = ''.join(sorted(list(scrambled_key)))
+    encr_data = data.decode('hex')
+    decr_data = rc4(encr_data, key)
+
+    return decr_data
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'Usage: %s <post url param/location> <post data>' % sys.argv[0]
+        exit()
+    else:
+        print decrypt_data(sys.argv[1], sys.argv[2])
